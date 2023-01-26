b08cebd9cc60552d5a36cf85b82c3a90c897d146
G-eugenio <geugnio12@gmail.com>
2023-01-26 11:32:04 -0300

    Arquivo novo: Script

---
+++ script.js
@@ -0,0 +1,9 @@
+// let entrada = parseInt(prompt("Digite o ano que voce nasceu:"));
+// alert(2003 - entrada);
+// alert(typeof entrada);
+
+let section = document.querySelector("section");
+
+let header = document.querySelector("header");
+header.classList.add("dark-theme");
+section.classList.add("dark-theme");
