05e7ad04f34d739452567663e8093108369004e3
G-eugenio <geugnio12@gmail.com>
2023-01-26 11:51:03 -0300

    javascript2

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // let entrada = parseInt(prompt("Digite o ano que voce nasceu:"));
 // alert(2003 - entrada);
 // alert(typeof entrada);
-
 let section = document.querySelector("section");
+function toDarkSide() {
+  section.classList.add("dark-theme");
+  section.classList.remove("light-theme");
+}
 
-let header = document.querySelector("header");
-header.classList.add("dark-theme");
-section.classList.add("dark-theme");
+function toJediSide() {
+  section.classList.add("light-theme");
+  section.classList.remove("dark-theme");
+}
