1bff543163e328ab56686ec1c0767866619717ca
Gabriel Eugenio <geugnio12@gmail.com>
2023-02-01 17:21:47 -0300

    mudança botao js

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
 // alert(2003 - entrada);
 // alert(typeof entrada);
 let section = document.querySelector("section");
-function toDarkSide() {
-  section.classList.add("dark-theme");
-  section.classList.remove("light-theme");
-}
+let header = document.querySelector("header");
+// function toDarkSide() {
+//   section.classList.add("dark-theme");
+//   section.classList.remove("light-theme");
+// }
 
-function toJediSide() {
-  section.classList.add("light-theme");
-  section.classList.remove("dark-theme");
+// function toJediSide() {
+//   section.classList.add("light-theme");
+//   section.classList.remove("dark-theme");
+// }
+
+function switchDarkLight() {
+  section.classList.toggle("dark-theme");
+  header.classList.toggle("dark-theme");
 }
