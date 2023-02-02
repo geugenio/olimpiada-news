f5d2b68e03c7274decafbf576f6720b5940589ee
geugenio <geugenio>
2023-02-02 11:05:06 -0300

    prova teste 1

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// let entrada = parseInt(prompt("Digite o ano que voce nasceu:"));
-// alert(2003 - entrada);
-// alert(typeof entrada);
-let section = document.querySelector("section");
-let header = document.querySelector("header");
-// function toDarkSide() {
-//   section.classList.add("dark-theme");
-//   section.classList.remove("light-theme");
-// }
-
-// function toJediSide() {
-//   section.classList.add("light-theme");
-//   section.classList.remove("dark-theme");
-// }
-
+let body = document.querySelector("body");
 function switchDarkLight() {
-  section.classList.toggle("dark-theme");
-  header.classList.toggle("dark-theme");
+  body.classList.toggle("dark-theme");
 }
